c9250ee4b07098e56d250f3b6113b725e584b094
Fabian <fabian@klaffenboeck.eu>
2024-04-08 17:47:10 +0200

    added code and lib for theoretically getting started

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <Arduino.h>
+#include <TinyGPS++.h>
+
+// Serial and decoding for GPS
+HardwareSerial ss(0);
+TinyGPSPlus gps;
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    Serial.print("code stared");
+
+    ss.begin(9600);
+    ss.setPins(32, 33);
 }
 
 void loop() {
-// write your code here
+    while (ss.available() > 0) {
+        gps.encode(ss.read());
+        if (gps.location.isUpdated()) {
+            Serial.print("Latitude= ");
+            Serial.println(gps.location.lat(), 6);
+            Serial.print("Longitude= ");
+            Serial.println(gps.location.lng(), 6);
+            Serial.print("Date= ");
+            Serial.println(gps.date.day(), 6);
+        }
+    }
 }
