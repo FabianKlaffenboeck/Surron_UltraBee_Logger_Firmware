bdbe9c24ac1d5a07f5d0b9eda7e26607f0d8fba0
Fabian <fabian@klaffenboeck.eu>
2024-04-08 18:47:24 +0200

    gps  send data or sth

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,18 @@
 #include "AsyncGpsHandler/AsyncGpsHandler.h"
 
 // Serial and decoding for GPS
-HardwareSerial ss(1);
+HardwareSerial ss(2);
 TinyGPSPlus gps;
 
 void setup() {
     Serial.begin(9600);
     Serial.print("code stared");
 
-    ss.begin(9600);
-    ss.setPins(32, 33);
+    ss.begin(9600, SERIAL_8N1, 32, 33);
 }
 
 void loop() {
     while (ss.available() > 0) {
-        gps.encode(ss.read());
-        if (gps.location.isUpdated()) {
-            Serial.print("Latitude= ");
-            Serial.println(gps.location.lat(), 6);
-            Serial.print("Longitude= ");
-            Serial.println(gps.location.lng(), 6);
-            Serial.print("Date= ");
-            Serial.println(gps.date.day(), 6);
-        }
+        Serial.write(ss.read());
     }
 }
