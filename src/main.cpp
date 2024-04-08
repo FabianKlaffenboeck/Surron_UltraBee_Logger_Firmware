abc90a95d1013e8af52ffbab171925ddf8ac77b9
Fabian <fabian@klaffenboeck.eu>
2024-04-08 18:57:01 +0200

    dont know whats up

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,17 @@ TinyGPSPlus gps;
 
 void setup() {
     Serial.begin(9600);
-    Serial.print("code stared");
-
     ss.begin(9600, SERIAL_8N1, 32, 33);
 }
 
 void loop() {
     while (ss.available() > 0) {
-        Serial.write(ss.read());
+        gps.encode(ss.read());
+        if (gps.location.isUpdated()) {
+            Serial.print("Latitude=");
+            Serial.print(gps.location.lat(), 6);
+            Serial.print("Longitude=");
+            Serial.println(gps.location.lng(), 6);
+        }
     }
 }
