b9eb64eb8dc7ae8dc3334fc86efe3f866e37eaea
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2024-11-02 21:12:49 +0100

    removed wifi ota

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,15 @@
 #include <Arduino.h>
 
-#include <WiFi.h>
-#include <WebServer.h>
-#include <ElegantOTA.h>
-
 #include "Drivers/LteDriver/LteDriver.h"
 #include "Drivers/GpsDriver/GpsDriver.h"
 #include "Drivers/CanDriver/CanDriver.h"
 #include "Drivers/IO_ReadDriver/IO_ReadDriver.h"
 #include "MqttHandler/MqttHandler.h"
-//#include "Secrets_TEMPLATE.h"
-#include "Secrets.h"
+#include "Secrets_TEMPLATE.h"
+// #include "Secrets.h"
 #include "CanParsers/VehicleDataParser/VehicleDataParser.h"
 #include "models/VehicleData.h"
 #include "Settings.h"
-
-WebServer server(80);
-const char *ssid = "ESP32-Access-Point";
-const char *password = "123456789";
 
 unsigned long lastMqttPubTime = 0;
 
@@ -37,27 +29,9 @@ void CanCallback(CanMsg canMsg) {
     vehicleDataParser.pars(canMsg);
 }
 
-void setupOTA() {
-    WiFi.softAP(ssid, password);
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP);
-    server.on("/", []() {
-        server.send(200, "text/plain", "Hi! This is ElegantOTA Demo.");
-    });
-    ElegantOTA.begin(&server);
-    server.begin();
-}
-
-void otaLoop() {
-    server.handleClient();
-    ElegantOTA.loop();
-}
 
 void setup() {
     Serial.begin(115200);
-
-    setupOTA();
 
     ioReadDriver.init(32);
     lteDriver.connect();
@@ -68,8 +42,6 @@ void setup() {
 }
 
 void loop() {
-
-    otaLoop();
 
     CanHandlerLoop();
     lteDriver.loop();
